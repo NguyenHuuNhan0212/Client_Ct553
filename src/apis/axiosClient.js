838c1c6349f240e2cc3eac168144d522534236e5
NguyenHuuNhan0212 <nhanvigo@gmail.com>
2025-11-18 10:08:01 +0700

    test deploy

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 // Tạo instance axios
+const api = import.meta.env.VITE_API_URL;
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: `${api}/api`,
   headers: {
     'Content-Type': 'application/json'
   },
