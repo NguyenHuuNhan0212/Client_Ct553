f5b9e4217e4e54854ac46551340a8375060195e4
NguyenHuuNhan0212 <nhanvigo@gmail.com>
2025-11-18 10:31:15 +0700

    update finish test

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 // Tạo instance axios
-const api = import.meta.env.VITE_API_URL;
 const axiosClient = axios.create({
-  // baseURL: 'http://localhost:3000/api',
-  baseURL: `${api}/api`,
+  baseURL: 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json'
   },
